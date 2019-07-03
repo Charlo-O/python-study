937bc3b141cd1bf07ba96d8574dca4a3562ca8b6
Charlo <49369951+Charlo-O@users.noreply.github.com>
2019-07-04 00:15:11 +0800

    Update 豆瓣读书首页标题.py

--- 豆瓣读书首页标题.py
+++ 豆瓣读书首页标题.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[38]:
-
-
 # 先导入我们需要的库
 import requests
 import re
 from bs4 import BeautifulSoup
+
 #发送“请求”
 kv = {'User-Agent': 'Mozilla/5.0'}
 r = requests.get('https://book.douban.com/top250?icn=index-book250-all', headers = kv)
 demo = r.text
 soup = BeautifulSoup(demo, 'html.parser')
-
 
 # 从解析文件中通过select选择器定位指定的元素，返回一个列表
 titles = soup.select('div.pl2 > a ')
@@ -24,16 +21,3 @@ for i in titles:
        
     print(title) 
         
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
